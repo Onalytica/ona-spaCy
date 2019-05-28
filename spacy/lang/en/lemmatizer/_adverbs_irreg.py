47b22cb8dacb93d71cc228e154f362eab535bab8
Kaushik SureshKumar <KSureshkumar@cp.onalytica.com>
2019-05-28 11:00:51 +0100

    add weirdly lemmatised words, specifically ai

--- spacy/lang/en/lemmatizer/_adverbs_irreg.py
+++ spacy/lang/en/lemmatizer/_adverbs_irreg.py
@@ -3,11 +3,16 @@ from __future__ import unicode_literals
 
 
 ADVERBS_IRREG = {
+    "artificial": ("artificial",),
+    "artificialintelligence": ("artificialintelligence",),
     "best": ("well",),
     "better": ("well",),
     "deeper": ("deeply",),
+    "deeplearning": ("deeplearning",),
     "farther": ("far",),
+    "fintech": ("fintech",),
     "further": ("far",),
     "harder": ("hard",),
-    "hardest": ("hard",)
+    "hardest": ("hard",),
+    "machinelearning": ("machinelearning",),
 }
